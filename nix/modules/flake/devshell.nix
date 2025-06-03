ab3c79807d48db8b9a17d78b31b274cd9efb3f7f
sonowz <dnsdhrj123@gmail.com>
2025-06-03 12:32:40 +0900

    Add nix support

---
+++ nix/modules/flake/devshell.nix
@@ -0,0 +1,18 @@
+{
+  perSystem = { config, pkgs, ... }: {
+    # Default shell.
+    devShells.default = pkgs.mkShell {
+      name = "haskell-google-genai-client";
+      meta.description = "Haskell development environment";
+      # See https://community.flake.parts/haskell-flake/devshell#composing-devshells
+      inputsFrom = [
+        config.haskellProjects.default.outputs.devShell # See ./nix/modules/haskell.nix
+        config.pre-commit.devShell # See ./nix/modules/formatter.nix
+      ];
+      packages = with pkgs; [
+        nixd
+        ghciwatch
+      ];
+    };
+  };
+}
